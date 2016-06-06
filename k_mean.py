ef71ac615673c0cac1059142e37e35cfda34de37
dmitrysarov <dmvish@yandex.ru>
2016-06-06 19:22:46 +0400

    Update k_mean.py

--- k_mean.py
+++ k_mean.py
@@ -59,6 +59,7 @@ def kmean(points, centers):
 
 
 # for yinyang
+
 def global_filter(point_low_bound, point_upper_bound, point_max_noncenter_shift, point_cluster_center_shift):
     #global filtering. retun numbers of passed and filtered points
     numbers_of_nonfiltered_points_bool = point_low_bound - point_max_noncenter_shift >= point_upper_bound + point_cluster_center_shift
@@ -122,33 +123,61 @@ def glob_yingyang(points, centers):
         # print 'filtered point ', 100*(points.shape[0]- point_after_filter.shape[0])/points.shape[0], '%'
         centers = np.copy(new_centers)
     return clusters
+
 # For group and local filtering yingyang
+
 def creat_groups(centers, number_of_groups):
 #  for clusters centers defining groups as kmean clustering operation (5 iterations, as in article). returne cluster (group) number for each center 
     group_centers = centers[np.array(random.sample(range(centers.shape[0]), number_of_groups))]
     center_cluster = get_cluster_number(centers, group_centers)
 #    clusters_centers_shift = 1
-    new_group_centers = np.zeros(centers.shape)
+    new_group_centers = np.zeros([number_of_groups, 2])
     counter = 0
     while counter != 5:
         counter += 1
-        for i in xrange(centers.shape[0]):
+        for i in xrange(number_of_groups):
             new_group_centers[i] = np.mean(centers[:][center_cluster == i], axis=0)
 #        clusters_centers_shift = dist(new_centers, centers)
         center_cluster = get_cluster_number(centers, new_group_centers)
-        group_centers = np.copy(new_group_centers)
+        # group_centers = np.copy(new_group_centers)
     return center_cluster
+
 def group_max_shift(old_centers,new_centers, center_cluster, number_of_groups):
-#  return max  cluster shift per group. Should exclude center of cluster for witch point belongs
+#  return max  cluster shift per group. exclude center of cluster for witch point belongs
     centers_shift = dist(new_centers,old_centers)
-    max_group_shift = np.zeros([1 number_of_groups])
+    max_group_shift = np.zeros(number_of_groups)
     for i in xrange(number_of_groups):
         max_group_shift[i] = np.max(centers_shift[center_cluster == i])
     return max_group_shift
-        
+
+def points_low_bound_per_group(points, centers, center_cluster, number_of_groups):
+    # return low bounds for each point per group of clusters
+    low_bounds = np.zeros([points.shape[0], number_of_groups])
+    for i in xrange(number_of_groups):
+        for j in xrange(points.shape[0]):
+            centers_in_group = centers[center_cluster==i]
+            low_bounds[j,i]=np.min(dist(centers_in_group,np.repeat([points[j]],centers_in_group.shape[0],axis=0)))
+    return low_bounds
+
+
       
-def goup_local_yingyang(points,centers,number_of_groups):
+def group_local_yingyang(points,centers,number_of_groups):
+    clusters = get_cluster_number(points, centers)
     center_cluster = creat_groups(centers, number_of_groups)
+    low_bounds = points_low_bound_per_group(points, centers, center_cluster, number_of_groups)
+    new_centers = np.zeros(centers.shape)
+    point_to_center_dist = dist(points,np.array([centers[i] for i in clusters]))
+    for i in xrange(centers.shape[0]):
+        new_centers[i] = np.mean(points[clusters == i],axis=0)
+    grp_max_shft = group_max_shift(centers, new_centers, center_cluster, number_of_groups)
+    center_shift = dist(new_centers,centers)
+    point_center_shift = np.array([center_shift[i] for i in clusters])
+    passed_groups = np.array([low_bounds[:,i] - grp_max_shft[i] < point_to_center_dist + point_center_shift for i in xrange(number_of_groups)]).transpose()
+    passed_groups = np.array([np.nonzero(passed_groups[i]) for i in xrange(points.shape[0])])
+    print passed_groups
+
+
+
     
 # testing
 def tree_dementional_space_example():
@@ -206,7 +235,12 @@ def two_dementional_space_example():
     plt.show()
 def main():
 #    tree_dementional_space_example()
-     two_dementional_space_example()
+#      two_dementional_space_example()
+
+    points = np.random.normal(0, 1, [10, 2])
+    centers = points[np.array(random.sample(range(points.shape[0]), 4))]
+    number_of_groups = 2
+    group_local_yingyang(points,centers,number_of_groups)
 
 if __name__ == "__main__":
     main()
